9ac4badd80c990da7476402d3250af1b659098e8
Eddie4k-code <98666468+Eddie4k-code@users.noreply.github.com>
2023-07-04 14:16:10 -0400

    Added Recipe Search UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,6 +20,7 @@ function App() {
                 <Routes>
                     <Route path="/register" element={<Register />} />
                     <Route path="/login" element={<Login />} />
+                    <Route path="/" element={<Landing />} />
                 </Routes>
                 </BrowserRouter>
             </AuthContextProvider>
--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,31 +1,29 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 
 const Landing = () => {
+  const [searchQuery, setSearchQuery] = useState('');
 
+  const handleSearch = () => {
+    // Implement your search logic here
+    // You can make an API call to retrieve recipes based on the searchQuery
+  };
 
-    return (
-
-        <div className="landing-page">
-
-            <header>
-
-                <h1 className="logo">NutriHelper</h1>
-
-            </header>
-
-
-        </div>
-
-
-
-
-    ) 
-
-
-
-
+  return (
+    <div className="recipe-search-page">
+      <h1>Recipe Search</h1>
+      <div className="search-bar">
+        <input
+          type="text"
+          className="recipe-main-search"
+          placeholder="Search for recipes"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button className="recipe-main-search-btn" onClick={handleSearch}>Search</button>
+      </div>
+      {/* Display the search results here */}
+    </div>
+  );
 };
-
 
 export default Landing;
